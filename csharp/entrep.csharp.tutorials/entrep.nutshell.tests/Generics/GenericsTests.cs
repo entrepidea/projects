fc5cb5c1353dc13c459317b0ac4f3f1c18084478
jon yee <jonanthan.eahy.yee@gmail.com>
2016-05-14 17:38:37 -0400

    add c# practices and tutorials

---
+++ csharp/entrep.csharp.tutorials/entrep.nutshell.tests/Generics/GenericsTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1.Generics
+{
+    [TestClass]
+    public class GenericsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //declaring an int array
+            MyGenericArray<int> intArray = new MyGenericArray<int>(5);
+
+            //setting values
+            for (int c = 0; c < 5; c++)
+            {
+                intArray.setItem(c, c * 5);
+            }
+
+            //retrieving the values
+            for (int c = 0; c < 5; c++)
+            {
+                Console.Write(intArray.getItem(c) + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
