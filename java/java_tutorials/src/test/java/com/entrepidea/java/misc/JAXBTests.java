73ab5193491f15231f61e4a74411703146f61323
entrepidea <support@entrepidea.com>
2016-08-26 15:29:14 -0400

    jaxb unit test (empty)

---
+++ java/java_tutorials/src/test/java/com/entrepidea/java/misc/JAXBTests.java
@@ -0,0 +1,17 @@
+package com.entrepidea.java.misc;
+/**
+This is the test of JAXB, see the full recount in here
+http://entrepidea.com/blogs/tech/index.php/2016/08/26/jaxb-and-jibx/
+*/
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JAXBTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
