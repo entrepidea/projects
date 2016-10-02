d262838451478c38404faf36dbea70e84fe32fe3
entrepidea <support@entrepidea.com>
2016-10-02 14:45:40 -0400

    bootstrap tests

---
+++ java/spring_tutorials/src/main/java/com/entrepidea/spring/core/ioc/bootstraps/SpringBootStrapper1.java
@@ -0,0 +1,22 @@
+package com.entrepidea.spring.core.ioc.bootstraps;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+public class SpringBootStrapper1 {
+    private static final Logger log = LoggerFactory.getLogger(SpringBootStrapper1.class);
+    public static void main(String[] args){
+        //pay attention to see how directory is located in relation to the project.
+        //use prefix "file:" if you want to use absolution path to the config xml file.
+        //multiple config files can be loaded using wildcard *, as shown below.
+        FileSystemXmlApplicationContext ac = new FileSystemXmlApplicationContext
+        		("/src/main/resources/config/META-INF/spring/spring-bootstrap.xml");
+        ac.registerShutdownHook();
+        ac.refresh();
+        SimpleDriverDataSource ds = (SimpleDriverDataSource)ac.getBean("dataSource");
+        log.info("user name:={}",ds.getUsername());
+        ac.close();
+    }
+}
---
+++ java/spring_tutorials/src/main/java/com/entrepidea/spring/core/ioc/bootstraps/supports/Client.java
@@ -0,0 +1,28 @@
+package com.entrepidea.spring.core.ioc.bootstraps.supports;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+public class Client {
+	@Autowired
+	private Environment env;
+	@Value("${bean.name}")
+	private String beanName;
+	@Autowired
+	SimpleDriverDataSource dataSource;
+
+	public String getDasourceName() {
+		return dataSource.getUsername();
+	}
+
+	public Environment getEnv() {
+		return env;
+	}
+
+	public String getName() {
+		return beanName;
+	}
+
+}
