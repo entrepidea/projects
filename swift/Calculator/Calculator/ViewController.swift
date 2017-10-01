e247e7beb3e190e6263df53d2ccfb55ad8814705
entrepidea <support@entrepidea.com>
2017-10-01 17:21:56 -0400

    enumeration

--- swift/Calculator/Calculator/ViewController.swift
+++ swift/Calculator/Calculator/ViewController.swift
@@ -9,17 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userInMiddleofTyping = false
+    
+    private var brain = CalculatorBrain()
+    
+    //compute value
+    var displayVale : Double{
+        get{
+            return Double(display.text!)!
+        }
+        set{
+            display.text = String(newValue)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit: String = sender.currentTitle!
+        let currentDisplayText = display.text!
+        if userInMiddleofTyping {
+            display.text = currentDisplayText + digit
+        }
+        else{
+            display.text = digit
+            userInMiddleofTyping = true
+        }
+        
+        //check if the text is a sole '.', then the operand is finished setting yet.
+        if display.text! == "."{
+            display.text = String("0.")
+            return
+        }
+        brain.setOperand(displayVale)
     }
-
-
+    
+    @IBAction func performOption(_ sender: UIButton) {
+        userInMiddleofTyping = false
+        if let symbo = sender.currentTitle
+        {
+            
+            brain.performOperand(symbo)
+            displayVale = brain.result!
+        }
+    }
+    
+    //this occurs when +,-,* or / button is clicked
+    @IBAction func onBinaryOperator(_ sender: UIButton) {
+        userInMiddleofTyping = false
+        if let symbo = sender.currentTitle{
+            brain.setBinaryOperator(symbo)
+        }
+        
+    }
+    
 }
 
