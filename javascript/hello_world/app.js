0569440bb06d4ac4388211960d2536ecbdb61b7d
entrepidea <support@entrepidea.com>
2018-08-08 00:10:27 -0400

    javascript project

---
+++ javascript/hello_world/app.js
@@ -0,0 +1,10 @@
+
+function hellow(){
+    console.log('hello, world from javascript!');
+}
+
+function convert(){
+    console.log(convertToMB(1024));
+}
+
+$(document).ready(convert);
---
+++ javascript/hello_world/util.js
@@ -0,0 +1,3 @@
+convertToMB = function(bytes){
+    return (parseInt(bytes, 10) / 1024 / 1024).toFixed(2) + ' MB';
+}
