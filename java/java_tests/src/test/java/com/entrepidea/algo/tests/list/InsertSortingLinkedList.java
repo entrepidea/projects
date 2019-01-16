da295977df86f04d6d325d48d348161ed686604c
entrepidea <support@entrepidea.com>
2019-01-16 14:55:21 -0500

    Update test3 of insertSortingLinkedList; update java_tests/srt 2/main/java/jide/DockedTextEditor.java

--- java/java_tests/src/test/java/com/entrepidea/algo/tests/list/InsertSortingLinkedList.java
+++ java/java_tests/src/test/java/com/entrepidea/algo/tests/list/InsertSortingLinkedList.java
@@ -74,6 +74,54 @@ public class InsertSortingLinkedList {
     @Test
     public void test3(){
         //https://www.geeksforgeeks.org/insertion-sort-for-singly-linked-list/
-        //TODO
+        ListNode head = llu.constructListFromArray(new int[]{27,20,37,64,33,66});
+
+        //Print the original linked list.
+        llu.printList(head);
+
+        //If the list has only zero or one node, there is no need to sort.
+        if (head == null || head.next == null)
+            return;
+
+        //Starting from the second node, try to insert it to the already partial-sorted array,
+        //for example, we would to insert 20 to 27->null,
+        //then insert 37 to 20->27->null and so on.
+        ListNode toBeIns = head.next; //toBeIns means the node to be inserted to partially sorted linked list.
+        head.next = null; //artificially cut the linked list into two lists:
+           // one sorted starting from head,
+           // the other are the nodes to be inserted, starting from toBeIns.
+        ListNode nextToBeIns;
+        ListNode curr; //the current node in the linked list
+        ListNode prev; //the node before the current node
+        while (toBeIns != null) {
+            //insert toBeIns to the partially sorted linked list from head to the node before toBeIns
+            curr = head;
+            prev = null;
+
+            while (curr != null && curr.val < toBeIns.val) {
+                prev = curr;
+                curr = curr.next;
+            }
+            //two possibilities:
+            //1. curr == null,
+            //   this means the value of node to be inserted (toBeIns) is largest in the partially sorted linked list
+            //   and we just need to add toBeIns to the end.
+            //2. curr != null and curr.val >= toBeIns.val, we need to insert toBeIns right before curr
+
+            nextToBeIns = toBeIns.next; //update the node to be inserted in the next round
+            toBeIns.next = curr; //insert toBeIns to the front of curr.
+
+            //Insert toBeIns to the end of the prev. Note that prev may be null,
+            //in that case, we need to update the head of the sorted linked list.
+            if (prev == null)
+                head = toBeIns;
+            else
+                prev.next = toBeIns;
+
+            toBeIns = nextToBeIns;
+        }
+
+        //Print the sorted linked list.
+        llu.printList(head);
     }
 }
