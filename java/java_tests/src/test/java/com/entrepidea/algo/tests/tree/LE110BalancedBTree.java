68c8879e5a14cdc8d2980d7af9076afa6d94e6ad
entrepidea <support@entrepidea.com>
2019-08-12 23:32:11 -0400

    previous version was purely wrong.

--- java/java_tests/src/test/java/com/entrepidea/algo/tests/tree/LE110BalancedBTree.java
+++ java/java_tests/src/test/java/com/entrepidea/algo/tests/tree/LE110BalancedBTree.java
@@ -1,6 +1,7 @@
 package com.entrepidea.algo.tests.tree;
 
 import com.entrepidea.algo.tests.tree.supports.TreeNode;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import sun.reflect.generics.tree.Tree;
@@ -19,19 +20,6 @@ import java.util.*;
  * */
 public class LE110BalancedBTree {
 
-    TreeNode root;
-    @Before
-    public void setup(){
-        root = new TreeNode(3);
-        TreeNode curr = root;
-        curr.lChild = new TreeNode(9);
-        curr.rChild = new TreeNode(20);
-        curr = curr.rChild;
-        curr.lChild = new TreeNode(15);
-        curr.rChild = new TreeNode(7);
-        //curr = curr.rChild;
-        //curr.rChild = new TreeNode(13);
-    }
 
     private int getHeight(TreeNode node){
         if (node==null){
@@ -43,97 +31,46 @@ public class LE110BalancedBTree {
         return 1 + Math.max(lh, rh);
     }
 
-    @Test
-    public void testTreeHeight(){
-        System.out.println(getHeight(root.rChild));
-    }
-
-    class LevelHeight{
-        public int level;
-        public int height;
-        public LevelHeight(int l, int h){
-            level = l;
-            height = h;
+    private boolean isBalanced(TreeNode node){
+        if (node==null){
+            return true;
         }
-    }
+        int lh = getHeight(node.lChild);
+        int rh = getHeight(node.rChild);
 
-    public boolean checkBalancedBinaryTree(){
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        TreeNode node;
-        Map<TreeNode, LevelHeight> nodeLevelHeightMap = new HashMap<>();
-        Map<Integer, List<TreeNode>> levelMap = new HashMap<>();
-        nodeLevelHeightMap.put(root, new LevelHeight(1, getHeight(root)));
-        List<TreeNode> l = new ArrayList<>();
-        l.add(root);
-        levelMap.put(1,l);
-        while((node = q.poll())!=null){
-            if(node.lChild!=null){
-                if(nodeLevelHeightMap.get(node.lChild)==null){
-                    int height = getHeight(node.lChild);
-                    int level = nodeLevelHeightMap.get(node).level;
-                    level +=1;
-                    nodeLevelHeightMap.put(node.lChild, new LevelHeight(level, height));
-                    if(levelMap.get(level)!=null){ //found siblings.
-                        List<TreeNode> siblings = levelMap.get(level);
-                        for(Iterator<TreeNode> iter = siblings.iterator();iter.hasNext();){
-                            TreeNode n = iter.next();
-                            int h = nodeLevelHeightMap.get(n).height;
-                            int h2 = nodeLevelHeightMap.get(node.lChild).height;
-                            if((Math.abs(h2-h))>1){
-                                return true;
-                            }
-                        }
-                        siblings.add(node.lChild);
-                        levelMap.put(level, siblings);
-                    }
-                    else{ //not found siblings
-                        List<TreeNode> li = new ArrayList<>();
-                        li.add(node.lChild);
-                        levelMap.put(level, li);
-                    }
-                }
-                q.add(node.lChild);
-            }
-            if(node.rChild!=null){
-                if(nodeLevelHeightMap.get(node.rChild)==null){
-                    int height = getHeight(node.rChild);
-                    int level = nodeLevelHeightMap.get(node).level;
-                    level +=1;
-                    nodeLevelHeightMap.put(node.rChild, new LevelHeight(level, height));
-                    if(levelMap.get(level)!=null){ //found siblings.
-                        List<TreeNode> siblings = levelMap.get(level);
-                        for(Iterator<TreeNode> iter = siblings.iterator();iter.hasNext();){
-                            TreeNode n = iter.next();
-                            int h = nodeLevelHeightMap.get(n).height;
-                            int h2 = nodeLevelHeightMap.get(node.rChild).height;
-                            if((Math.abs(h2-h))>1){
-                                return true;
-                            }
-                        }
-                        siblings.add(node.rChild);
-                        levelMap.put(level, siblings);
-                    }
-                    else{
-                        List<TreeNode> li = new ArrayList<>();
-                        li.add(node.rChild);
-                        levelMap.put(level, li);
-                    }
-                }
-                q.add(node.rChild);
-            }
+        if(Math.abs(lh-rh)<=1 && isBalanced(node.lChild) && isBalanced(node.rChild)){
+            return true;
         }
-
         return false;
     }
 
     @Test
-    public void test(){
-        if(checkBalancedBinaryTree()){
-            System.out.println("Found!");
-        }
-        else{
-            System.out.println("Not Found!");
-        }
+    public void test2(){
+        TreeNode root;
+        root = new TreeNode(3);
+        TreeNode curr = root;
+        curr.lChild = new TreeNode(9);
+        curr.rChild = new TreeNode(20);
+        curr = curr.rChild;
+        curr.lChild = new TreeNode(15);
+        curr.rChild = new TreeNode(7);
+        //curr = curr.rChild;
+        //curr.rChild = new TreeNode(13);
+        Assert.assertTrue(isBalanced(root));
+    }
+
+    @Test
+    public void test3(){
+        TreeNode r = new TreeNode(1);
+        TreeNode cur = r;
+        cur.lChild = new TreeNode(2);
+        cur.rChild = new TreeNode(2);
+        cur = cur.lChild;
+        cur.lChild = new TreeNode(3);
+        cur.rChild = new TreeNode(3);
+        cur = cur.lChild;
+        cur.lChild = new TreeNode(4);
+        cur.rChild = new TreeNode(4);
+        Assert.assertFalse(isBalanced(r));
     }
 }
