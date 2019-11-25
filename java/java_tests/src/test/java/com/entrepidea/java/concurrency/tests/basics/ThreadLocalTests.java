7ee7c9981927e1baabf3c2f7ac2f2099872f516d
entrepidea <support@entrepidea.com>
2019-11-25 16:30:16 -0500

    add another ThreadLocal test case

--- java/java_tests/src/test/java/com/entrepidea/java/concurrency/tests/basics/ThreadLocalTests.java
+++ java/java_tests/src/test/java/com/entrepidea/java/concurrency/tests/basics/ThreadLocalTests.java
@@ -1,5 +1,7 @@
 package com.entrepidea.java.concurrency.tests.basics;
 
+import java.util.Random;
+import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -13,6 +15,8 @@ import org.slf4j.LoggerFactory;
  * @description ThreadLocal enables each thread to own a local copy of some variables thus prevent data tampering in a multi-threading environment.
  *
  * Synchronization is heavy, and extreme care is required. In many cases ThreadLocal provides a simple alternative.
+ *
+ * Be sure to read JCP 3.3.3
   *
  */
 
@@ -27,7 +31,7 @@ public class ThreadLocalTests {
         *
      */
     class SerialNumber {
-        private ThreadLocal<Integer> tl = null;
+        private ThreadLocal<Integer> tl;
         public SerialNumber(Integer id){
             tl = ThreadLocal.withInitial(()->id); //static method withInital was introduced since 1.8
         };
@@ -92,7 +96,69 @@ TODO: 10/15/14, Markit on site
 
 
     //BNP Paribas onsite, Jersey City, GWT UI programmer position, 10/14/2014
-    //TODO 9. ThreadLocal, how do you use it? How is it used with a session?
+    //ThreadLocal, how do you use it? How is it used with a session?
+    //The code below seems working, but once the Thread.sleep is removed, it seems becoming problematical.
+    //TODO testing more...
+    class SessionIdGenerator {
+        private ThreadLocal<UUID> sessionId;
+        public void generate(){
+            sessionId = ThreadLocal.withInitial(()->UUID.randomUUID());
+        }
+        public String getId(){
+            return sessionId.get().toString();
+        }
+    }
+
+    class SessionHandler implements Runnable{
+        private Logger log = LoggerFactory.getLogger(SessionHandler.class);
+        private SessionIdGenerator session;
+        public SessionHandler(SessionIdGenerator s){
+            session = s;
+            session.generate();
+        }
+
+        public void begin() throws InterruptedException{
+            Thread.sleep(new Random().nextInt(100));
+            log.info("Starting ... [Thread={}, session id={}]", Thread.currentThread().getName(), session.getId());
+        }
+
+        public void wip() throws InterruptedException{
+            Thread.sleep(new Random().nextInt(100));
+            log.info("Working in progress ... [Thread={}, session id={}]", Thread.currentThread().getName(), session.getId());
+        }
+
+        public void terminating() throws InterruptedException{
+            Thread.sleep(new Random().nextInt(100));
+            log.info("Session ends... [Thread={}, session id={}]", Thread.currentThread().getName(), session.getId());
+        }
+
+
+        @Override
+        public void run() {
+            try {
+                begin();
+                wip();
+                terminating();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void threadLocalDemo() throws InterruptedException{
+        //System.out.println(UUID.randomUUID().toString());
+        int threadNumber;
+        ExecutorService service = Executors.newFixedThreadPool(threadNumber = Runtime.getRuntime().availableProcessors());
+        SessionIdGenerator generator = new SessionIdGenerator();
+        for(int i=0;i<threadNumber;i++){
+            service.submit(new SessionHandler(generator));
+        }
+
+        service.awaitTermination(5, TimeUnit.SECONDS);
+
+    }
+
 
 
 }
