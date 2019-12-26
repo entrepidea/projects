2e6a149e551546fb2563ff69ef18f842c1ce6e02
entrepidea <support@entrepidea.com>
2019-12-26 16:13:21 -0500

    2019 account process python code

--- python/prod/account/2019/Main.py
+++ python/prod/account/2019/Main.py
@@ -1,11 +1,29 @@
 from sys import argv
-
+import os
+import re
+from datetime import datetime
+import pandas as pd
 """
 Utilities methods.
 """
+
+def num(s):
+    pat = re.compile(r'^[0-9]*[.,]?[0-9]*$')
+    if pat.match(s):
+        if ',' in s:
+            s = s.replace(',','')
+        try:
+            return int(s)
+        except ValueError:
+            return float(s)
+    else:
+        return 0
+
+#Convert a tuple to a string
 def unpack(tup):
     return ','.join(str(s) for s in tup).strip()
 
+#keyword 'yield' is used to create a generator, it's handy in some case as the one below.
 def matched(debit_tups, ref_tups):
     for item in debit_tups:
         found = False
@@ -16,6 +34,12 @@ def matched(debit_tups, ref_tups):
         if not found:
             yield item + tuple(['misc'])
 
+def def_dest_folder(d=None):
+    d = 'generated' if d is None else d
+    if not os.path.exists(d):
+        os.makedirs(d)
+    return d
+
 """
 Business methods
 """
@@ -23,30 +47,32 @@ def parse_transaction_file(trans_file):
     with open(trans_file) as f:
         contents = f.readlines()
         keyword = 'CHECK'
-        to_file_name = keyword+'.CSV'
+
+        to_file_name = def_dest_folder() + '\\CHASE_' + keyword+'.CSV'
         with open(to_file_name, 'w') as to:
-            to.writelines(line.strip()+'\n' for line in contents if line.split(',')[0]==keyword)
+            to.writelines(line.split(',')[1]+','+line.split(',')[2]+','+line.split(',')[3]+'\n'
+                          for line in contents if line.split(',')[0]==keyword and float(line.split(',')[3])!=-1402.45) # the number is monthly salary, it's excluded.
 
         keyword = 'CREDIT'
-        to_file_name = keyword+'.CSV'
+        to_file_name = def_dest_folder() + '\\CHASE_' + keyword+'.CSV'
         with open(to_file_name, 'w') as to:
             to.writelines(line.strip()+'\n' for line in contents if line.split(',')[0]==keyword)
 
         keyword = 'DEBIT'
-        to_file_name = keyword+'.CSV'
+        to_file_name = def_dest_folder() + '\\CHASE_' + keyword+'.CSV'
         with open(to_file_name, 'w') as to:
             to.writelines(line.strip()+'\n' for line in contents
                           if line.split(',')[0]==keyword
                           and 'CHASE CREDIT CRD AUTOPAYBUS' not in line.split(',')[2] #remove chase credit, analysis will be done seperatedly
                           and 'CITI AUTOPAY' not in line.split(',')[2]) #remove citi credit, analysis will be done seperatedly
 
-        return parse_debit_file('DEBIT.CSV')
+        return parse_debit_file(to_file_name)
 
 
 def parse_debit_file(file_name):
     with open(file_name) as f:
         contents = f.readlines()
-        debit_tups = sorted([(date, desc, amt) for _, date, desc, amt, _, _, _, _ in (line.split(',') for line in contents)],key=lambda tup:tup[1])
+        debit_tups = [(date, desc, amt) for _, date, desc, amt, _, _, _, _ in (line.split(',') for line in contents)]
 
     return debit_tups
 
@@ -55,36 +81,102 @@ def parse_debit_file(file_name):
 def parse_chase_credit_card_file(credit_card_file):
     with open(credit_card_file) as f:
         contents = f.readlines()
-        creditcard_tups = sorted([(date, desc, amt) for _,_,date,desc,_,_,amt,_ in (line.split(',') for line in contents[1:])],key=lambda tup:tup[1])
+        creditcard_tups = [(date, desc, amt) for _,_,date,desc,_,_,amt,_ in (line.split(',') for line in contents[1:] if 'THANK' not in line)]
+
+    with open (def_dest_folder() + '\\' + 'CHASE_CREDITCARD_THANK.CSV','w') as f:
+        f.writelines(line.strip() +'\n' for line in contents if 'THANK' in line)
 
     return creditcard_tups
 
 
-def write_expense_file(debitcard_tups, chase_creditcard_tups, file_name):
+def merge_expense_items(debitcard_tups, chase_creditcard_tups, citi_creditcard_tups, chase_check_tups):
     with open("reference.txt") as ref:
         refs = ref.readlines()
         ref_tups = sorted([(desc, cat) for desc, cat in (line.strip().split(',') for line in refs)],key=lambda tup:tup[1])
 
     debitcard_items = [unpack(i) for i in matched(debitcard_tups, ref_tups) ]
     chase_creditcard_items = [unpack(i) for i in matched(chase_creditcard_tups,ref_tups)]
+    citi_creditcard_items = [unpack(i) for i in matched(citi_creditcard_tups, ref_tups)]
+    chase_check_items = [unpack(i) for i in matched(chase_check_tups, ref_tups)]
 
-    all_expense_items = debitcard_items + chase_creditcard_items
+    all_expense_items = sorted(debitcard_items + chase_creditcard_items + citi_creditcard_items+chase_check_items, key=lambda line: (line.split(',')[3],line.split(',')[1]))
     [print(i) for i in all_expense_items]
 
+
+
+    return all_expense_items
+
+
+def aggregate_expense_items(all_expense_items, aggregate_output_file_name):
+
+
+    data = {"cat":[ line.split(',')[3] for line in all_expense_items],
+            "amnt":[float(line.split(',')[2]) for line in all_expense_items]}
+    df = pd.DataFrame(data)
+    #print(df)
+    grouped_total = df.groupby('cat').sum()
+    print(grouped_total)
+    grouped_total.to_csv(aggregate_output_file_name, sep=',', encoding='utf-8')
+
+def write_expense_file(all_expense_items, file_name):
     with open(file_name,'w') as to:
         to.writelines(i+'\n' for i in all_expense_items)
 
 
+
+def parse_citi_credit_card_file(file_name):
+    with open(file_name) as f:
+        contents = f.readlines()
+        #tups = [(date, desc, amt) for _,date,desc,amt,_,_ in (line.strip().split(',') for line in contents[6:])]
+        s = ' '.join([line.rstrip('\n') for line in f])
+
+
+
+    line_list = [line.split(',') for line in s.split("Cleared")]
+    #for line in line_list:
+    ret = []
+    for line in line_list:
+        if len(line)<5:
+            continue
+
+        if str(line[3]) != '':
+            line[3] = str(0-num(line[3].strip()))
+        if str(line[4]) !='':
+            line[3] = line[4]
+
+        t = (line[1],line[2],line[3])
+        ret.append(t)
+
+    return ret
+
+def parse_chase_checks_file(file_name):
+    with open(file_name) as f:
+        contents = f.readlines()
+        tups = [(date, desc, amt) for date, _, amt, desc in (line.strip().split(',') for line in contents)]
+        print(tups)
+    return tups
+
 def main(argu):
     if argu is None or len(argu) == 0:
         trans_file = 'chase_biz_account_all_transactions_2019.CSV'
-        credit_file = 'chase_biz_credit_card_expense_2019.CSV'
+        chase_credit_file = 'chase_biz_credit_card_expense_2019.CSV'
+        citi_credit_file = 'citi_credit_card_expense_2019.CSV'
+        chase_checks_file = def_dest_folder()+'\\CHASE_CHECK_enriched.CSV'
     else:
-        trans_file, credit_file = argu
+        trans_file, chase_credit_file,citi_credit_file = argu
 
     debitcard_items = parse_transaction_file(trans_file)
-    chase_credditcard_items = parse_chase_credit_card_file(credit_file)
-    write_expense_file(debitcard_items,chase_credditcard_items, 'processed.CSV')
+    chase_creditcard_items = parse_chase_credit_card_file(chase_credit_file)
+    citi_creditcard_items = parse_citi_credit_card_file(citi_credit_file)
+    chase_check_items =  parse_chase_checks_file(chase_checks_file)
+    all_expense_items = merge_expense_items(debitcard_items, chase_creditcard_items, citi_creditcard_items, chase_check_items)
+    output_filename = def_dest_folder() + '\\processed#1_'+datetime.now().strftime('%Y%m%d_%H%M%S')+'.CSV'
+    write_expense_file(all_expense_items, output_filename)
+
+    #using pandas to do some aggregation work.
+    output_filename = def_dest_folder() + '\\processed#2_' + datetime.now().strftime('%Y%m%d_%H%M%S') + '.CSV'
+    aggregate_expense_items(all_expense_items, output_filename)
+
 
 if __name__ == '__main__':
     main(argv[1:])
