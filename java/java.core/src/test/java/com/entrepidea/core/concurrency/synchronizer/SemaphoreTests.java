43b43357e7d64d2b7af18dbb005ae6df93c9c406
Jonathan Yee <support@entrepidea.com>
2020-02-28 00:02:05 -0500

    add example to demo semaphore

--- java/java.core/src/test/java/com/entrepidea/core/concurrency/synchronizer/SemaphoreTests.java
+++ java/java.core/src/test/java/com/entrepidea/core/concurrency/synchronizer/SemaphoreTests.java
@@ -5,7 +5,9 @@ package com.entrepidea.core.concurrency.synchronizer;
  * Counting Semaphores are used to control the number of the activities that can access certain resource or perform a given action at the same time.
  * A semaphore manages a set of permits, the method "acquire" blocks until there are available permit; and the method "release" adds a permit. If there is only one
  * permit, semaphore becomes a mutex with nonreentrant locking semantics; whoever holds the sole permit holds the mutex.
- * 
+ *
+ * Semaphore is used mostly to create a resource pool, controlling the availability of the resource in the pool. Like Connection pool, for instance.
+ * Another use case is to set the "bound" for a collection. An example can be found in CJP and showed below.
  *
  * @Date: Unknown, 02/27/20
  * 
@@ -23,13 +25,13 @@ import java.util.concurrent.TimeUnit;
 public class SemaphoreTests {
 
 	//The following demo one usage of semaphore: it's able to control the number of object in a set. One direct use might be found in creating a connection pool.
-	//reference: Java Concurrency in Practice, Brian Goetz, p98
+	//reference: Java Concurrency in Practice, Brian Goetz, 5.5.3 "Semaphore", p98
 	class BoundedSet<T>{
 		private Set<T> set;
 		private Semaphore sem;
 		public BoundedSet(int permits){
 			sem = new Semaphore(permits);
-			set = Collections.synchronizedSet(new HashSet<T>());
+			set = Collections.synchronizedSet(new HashSet<>());
 		}
 		public boolean add(T o) throws InterruptedException {
 			boolean wasAdded = false;
@@ -52,8 +54,26 @@ public class SemaphoreTests {
 		}
 	}
 	@Test
-	public void test(){
-		//TODO testing the class BoundedSet
+	public void test() throws InterruptedException {
+		BoundedSet<String> bs = new BoundedSet<>(5);
+		final int poolSize = 10;
+		ExecutorService es = Executors.newFixedThreadPool(poolSize);
+		for(int i=0;i<poolSize;i++){
+			es.execute(()->{
+				try {
+					String str = Thread.currentThread().getName();
+					while (!bs.add(str)) ;
+					System.out.println(str+ " added a string,");
+					Thread.sleep(1000);
+					System.out.println(str + " waited for 1 sec and release the permit");
+					bs.remove(str);
+				}
+				catch(InterruptedException e){
+					e.printStackTrace();
+				}
+			});
+		}
+		es.awaitTermination(10, TimeUnit.SECONDS);
 	}
 
 
