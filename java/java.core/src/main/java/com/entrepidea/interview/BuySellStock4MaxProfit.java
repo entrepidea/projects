816fab5c99d8d226df54830ce51a096cbd3e192a
Jonathan Yee <support@entrepidea.com>
2020-03-13 16:47:41 -0400

    new file

---
+++ java/java.core/src/main/java/com/entrepidea/interview/BuySellStock4MaxProfit.java
@@ -0,0 +1,22 @@
+package com.entrepidea.interview;
+/**
+ * @Desc:
+ * The details of the question can be found in here:
+ * https://www.geeksforgeeks.org/stock-buy-sell/
+ * However there seemed to have two variations of this problem:
+ * 1. Buy or sell only once each day
+ * 2. Buy 1 sell any times each day
+ * For the 2nd variation, my friend Victor Tan gave a solution in here:
+ * https://github.com/tiger40490/repo1/blob/py1/py/algo_arr/maxProfit_buy1sellAny.py
+ *
+ * @date: 03/13/20
+ *
+ * @Note:
+ * This question was asked on a 01/27/20 Goldman onsite interview and again in a Bank of New York HankRank test on 02/18/20.
+ *
+ * */
+
+public class BuySellStock4MaxProfit {
+    //TODO
+
+}
