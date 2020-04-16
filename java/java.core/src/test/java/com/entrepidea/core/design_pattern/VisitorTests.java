acc6d21cc1fd7943849f898843214309ddb0fec9
Jonathan Yee <support@entrepidea.com>
2020-04-16 13:14:33 -0400

    add visitor pattern tests

---
+++ java/java.core/src/test/java/com/entrepidea/core/design_pattern/VisitorTests.java
@@ -0,0 +1,17 @@
+package com.entrepidea.core.design_pattern;
+/**
+ * @Desc:
+ * Visitor pattern involves two abstract/interface types, with referencing to each other. When the method of one type is invoked, the strategy is implemented inside the other.
+ * JDK example include FileVistor and SimpleFileVisitor
+ *
+ * @Date: 04/16/20
+ *
+ * */
+public class VisitorTests {
+    /**
+     * BNP Paribas on-site, 02/28/20
+     * what's the visitor pattern?
+     * */
+    //Visitor pattern creates a separate visitor class to offer functionalities for a working class. Therefore, once new functions are added, the working class remains intact.
+    //Example: JDK's FileVisitor interface and SimpleFileVisitor class.
+}
