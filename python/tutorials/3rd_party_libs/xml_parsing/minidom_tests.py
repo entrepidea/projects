c0e976cb7e15bca128396d671db5bb9c03a8b99e
Jonathan Yee <support@entrepidea.com>
2020-05-10 19:28:56 -0400

    restructure python code

---
+++ python/tutorials/3rd_party_libs/xml_parsing/minidom_tests.py
@@ -0,0 +1,11 @@
+
+"""
+Desc: I came across xml.dom.minidom package in a BNP Paribas project. Here are some tests to familirize myself to this lib
+Reference: https://docs.python.org/3.6/library/xml.dom.minidom.html
+Note: I can't have xml.dom.minidom installed and I posted a question on SO: https://stackoverflow.com/questions/61719391/pycharm-2020-1-doesnt-find-minidom-in-python-3-6-in-linux-mint
+Date: 05/10/20
+"""
+#TODO
+# from xml.dom.minidom import parse, paring
+#dom3 = parseString('<myxml>Some data<empty/> some more data</myxml>')
+#assert dom3.documentElement.tagName == "myxml"
--- python/tutorials/datastructure_algo/tuple_tests.py
+++ python/tutorials/datastructure_algo/tuple_tests.py
@@ -1,33 +1,33 @@
 #Tuple is enclosed using parentnesses, while list uses bracket
 #Tuple's elements can't be changed so it's regarded as read-only list
 
-tuple = ( 'abcd', 786 , 2.23, 'john', 70.2  )
+tuple = ('abcd', 786 , 2.23, 'john', 70.2)
 tinytuple = (123, 'john')
 
 print(tuple)           # Prints complete list
 print(tuple[0])        # Prints first element of the list
-print (tuple[1:3])      # Prints elements starting from 2nd till 3rd
-print (tuple[2:])       # Prints elements starting from 3rd element
-print (tinytuple * 2)   # Prints list two times
-print (tuple + tinytuple) # Prints concatenated lists
+print(tuple[1:3])      # Prints elements starting from 2nd till 3rd
+print(tuple[2:])       # Prints elements starting from 3rd element
+print(tinytuple * 2)   # Prints list two times
+print(tuple + tinytuple) # Prints concatenated lists
 
 #delete tuple elements
 tup = ('physics', 'chemistry', 1997, 2000)
 
 #tuples is sequence and will be treated as such
-print ('tup[2] = '+ tup[1])
-print ('tup from 2nd element: ')
-print  (tup[1:])
-del tup;
+print('tup[2] = '+tup[1])
+print('tup from 2nd element: ')
+print(tup[1:])
+del tup
 #print tup #will report error as tup no longer exists
 
 
-print (3 in (1,2,3))
+print(3 in (1,2,3))
 for x in (1,2,3):
-    print (x)
+    print(x)
 
 #test built-in tuple functions
 tuple1, tuple2 = (123, 'xyz', 'zara'), (456, 'abc')
 
-print ("First tuple length : ", len(tuple1))
-print ("Second tuple length : ", len(tuple2))
+print("First tuple length : ", len(tuple1))
+print("Second tuple length : ", len(tuple2))
