3cc31d177ce2b0225bb70098ddb0c5c2129d508f
Jonathan Yee <support@entrepidea.com>
2021-01-05 16:33:42 -0500

    amended

--- python/tutorials/data_model/cookbook_cached_instance_8_25.py
+++ python/tutorials/data_model/cookbook_cached_instance_8_25.py
@@ -22,3 +22,28 @@ def get_spam(name):
         s = _spam_ref[name]
 
     return s
+
+
+# alternative to the above - use a manager for better and clearer management
+# pay attention to the class Spam2 and how it prevents being instantiated.
+class CachedSpamManager:
+    def __init__(self):
+        self._cache = weakref.WeakValueDictionary()
+
+    def get_spam(self, name):
+        if name not in self._cache:
+            s = Spam2._new(name)
+            self._cache[name] = s
+        else:
+            s = self._cache[name]
+        return s
+
+class Spam2:
+    def __init__(self):
+        raise RuntimeError("is not allowed to instantiate the class directly")
+
+    @classmethod
+    def _new(cls, name):
+        self = cls.__new__(cls)
+        self.name = name
+        return self
