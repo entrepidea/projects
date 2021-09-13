b4134f4515d23799b43ad3c2e7666ac160746d64
Jonathan Yee <support@entrepidea.com>
2021-09-13 11:57:11 -0400

    moving emantics in c++

---
+++ cpp/cpp_tests/moving_semantics_tests.cpp
@@ -0,0 +1,80 @@
+/*
+	move semantics is strongly associated with lvalue/rvalue thus a new feature in C++ 11. Its main purpose is to introduce a new constructor that eliminate the necessicity of creating another copy of the input object when a function is called.
+
+	resource: https://www.youtube.com/watch?v=ehMg6zvXuMY
+
+	create: 09/13/21
+
+ * */
+
+#include <iostream>
+#include <cstring>
+
+class String{
+public:
+	String() = default;				
+
+	String(const char* str){
+		printf("created!\n");						
+		m_size = strlen(str);
+		m_data = new char[m_size];
+		memcpy(m_data,str,m_size);		
+	}					
+
+	String(const String& other){
+		printf("copied constructor!\n");					
+		m_size = strlen(other.m_data);
+		m_data = new char[other.m_size];
+		memcpy(m_data,other.m_data,m_size);		
+		
+	}
+
+	//move constructor
+	String(String&& other) noexcept
+	{
+		printf("Moved constructor.\n");
+		m_size = other.m_size;
+		m_data = other.m_data;
+		other.m_size = 0;
+		other.m_data = nullptr;		
+	}
+
+	~String(){
+		printf("Destroyed!\n");					
+		delete m_data;
+	}
+
+
+	void print(){
+		for(uint32_t i = 0 ;i<m_size;i++){
+			printf("%c",m_data[i]);
+		}						
+		printf("\n");
+	}
+
+private:
+	uint32_t m_size;
+	char* m_data;
+		
+};
+
+//consumer class consuming String class.
+class Entity{
+public:
+	Entity(const String& name): m_name(name){}
+
+	Entity(String&& name):m_name((String&&)name){}
+
+	void printName(){
+		m_name.print();					
+	}
+
+private:
+	String m_name;	
+};
+
+int main(){
+	Entity entity(String("cherno"));				
+	entity.printName();
+	std::cin.get();
+}
