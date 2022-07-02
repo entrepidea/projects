8df186896dbc13829a314cf4087e07f89997d656
Jonathan Yee <support@entrepidea.com>
2022-07-01 20:41:37 -0400

    python update

---
+++ python/algo/leetcode/categories/README.txt
@@ -0,0 +1,4 @@
+this is from this article:
+https://www.zhihu.com/collection/749478404
+
+
--- python/algo/leetcode/categories/array/remove_element.py
+++ python/algo/leetcode/categories/array/remove_element.py
@@ -1,34 +1,46 @@
-def closed_range(start, stop, step=1):
-    dir = 1 if (step > 0) else -1
-    return range(start, stop + dir, step)
+"""
 
-def test_closed_range(arr):
-    for i in closed_range(0,len(arr)-1):
-        print(arr[i])
-    for i in range(0,len(arr)):
-        print(arr[i])
+https://www.cspiration.com/#/leetcodeClassification/codeDetail?id=27&language=en
+
+remove specified number from an array, return the shrunk array.
+
+date: 06/26/22
+
+"""
 
 def remove(arr,num) -> []:
     l = len(arr)
     found = 0
     i = 0
+    new_len = l
     while i < l:
         if arr[i] == num:
-            found += 1
-            for k in closed_range(i+1, l):
+            k = i+1
+            i = 0
+            while k < l:
                 arr[k-1] = arr[k]
-        else:
-            i += 1
-
-    new_len = l - found
-    return arr[0:new_len]
+                k += 1
+            l -= 1
+            #print(f'arr={arr[:l]},length={l},i={i}')
+        else:    
+            i+=1
+    return arr[:l]
 
 if __name__ == '__main__':
+    
     arr = [1,2,3,5,5]
     num = 5
-    #print(remove(arr,num))
-    #test_closed_range(arr)
-    arr = [1]
-    test_closed_range(arr)
-
-
+    print(f'origin arr={arr}, remove:{num}, final arr={remove(arr,num)}')
+    arr = [1,2,3,5,5]
+    num = 2
+    print(f'origin arr={arr}, remove:{num}, final arr={remove(arr,num)}')
+    arr = [1,2,3,5,5]
+    num = 1
+    print(f'origin arr={arr}, remove:{num}, final arr={remove(arr,num)}')
+    arr = [1,2,3,1,5]
+    num = 1
+    print(f'origin arr={arr}, remove:{num}, final arr={remove(arr,num)}')
+    
+    arr = [1,2,3,1,1]
+    num = 1
+    print(f'origin arr={arr}, remove:{num}, final arr={remove(arr,num)}')
