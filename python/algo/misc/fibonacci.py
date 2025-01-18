d31c19aeedd2a69c3b37347bc1f2961eb02f1013
Jon <support@entrepidea.com>
2025-01-18 11:01:59 -0500

    algo, fibonacci sequence

--- python/algo/misc/fibonacci.py
+++ python/algo/misc/fibonacci.py
@@ -7,7 +7,7 @@ Date: 01/16/25
 """
 
 result=[]
-for i in range(1,100001):
+for i in range(1,21):
 
      if i==1 or i==2:
          result.append(i)
@@ -15,7 +15,12 @@ for i in range(1,100001):
          x = result[i-2]+result[i-3]
          result.append(x)
 
+#print the fibonacci sequence for demo
+[print(i) for i in result]
+
+#find the last two elements of the sequence and calculate its ratio
 l = len(result)
 last_ele = result[l-1]
 sec_last_ele = result[l-2]
+print(f'Find last two elements [{last_ele},{sec_last_ele}] of the sequence and calculate its ratio:')
 print(last_ele/sec_last_ele)
