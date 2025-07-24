4454bf3b11e9c522b6a8f23accabd803c8eb04d0
jon <jon@miniforum.lan>
2025-07-23 23:25:36 -0400

    fix a bournary issue in bubble sorting

--- aaron_corner/python/algo/bubble_sorting.py
+++ aaron_corner/python/algo/bubble_sorting.py
@@ -14,14 +14,15 @@ def create_random_list(size, lower_bound, upper_bound):
 def sort(numbers):
     #iterated=numbers
     numbersIterated=len(numbers)-1
-    while numbersIterated>1:
+    while numbersIterated>0:
         for i in range(0,numbersIterated):
             if numbers[i]>numbers[i+1]:
                 numbers[i], numbers[i+1] = numbers[i+1], numbers[i]            
         numbersIterated-=1
 
 if __name__ == "__main__":  
-    numbers = create_random_list(9, 1, 30)
+    #numbers = create_random_list(9, 1, 30)
+    numbers = [30, 14, 21, 20, 23, 12, 29, 28, 7]
     print(f"Original:\n{numbers}")
     sort(numbers)
     print(f"Sorted:\n{numbers}")
