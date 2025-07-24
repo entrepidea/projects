b31009be0cae39ef54e206d1e9bbc349050fc246
jon <jon@miniforum.lan>
2025-07-23 23:27:00 -0400

    remove unnecessary comments

--- aaron_corner/python/algo/bubble_sorting.py
+++ aaron_corner/python/algo/bubble_sorting.py
@@ -12,7 +12,6 @@ def create_random_list(size, lower_bound, upper_bound):
     return [random.randint(lower_bound, upper_bound) for _ in range(size)]
 
 def sort(numbers):
-    #iterated=numbers
     numbersIterated=len(numbers)-1
     while numbersIterated>0:
         for i in range(0,numbersIterated):
@@ -21,8 +20,7 @@ def sort(numbers):
         numbersIterated-=1
 
 if __name__ == "__main__":  
-    #numbers = create_random_list(9, 1, 30)
-    numbers = [30, 14, 21, 20, 23, 12, 29, 28, 7]
+    numbers = create_random_list(9, 1, 30)
     print(f"Original:\n{numbers}")
     sort(numbers)
     print(f"Sorted:\n{numbers}")
