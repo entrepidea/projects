8c90620182040c7f9abe42dcb694df6d796090bd
jon <jon@miniforum.lan>
2025-07-23 23:20:19 -0400

    add a bubble sorting implementation

---
+++ aaron_corner/python/algo/bubble_sorting.py
@@ -0,0 +1,27 @@
+import random
+
+"""
+    
+    Bubble Sort Implementation
+    Aaron
+    07/23/2025
+
+"""
+
+def create_random_list(size, lower_bound, upper_bound):
+    return [random.randint(lower_bound, upper_bound) for _ in range(size)]
+
+def sort(numbers):
+    #iterated=numbers
+    numbersIterated=len(numbers)-1
+    while numbersIterated>1:
+        for i in range(0,numbersIterated):
+            if numbers[i]>numbers[i+1]:
+                numbers[i], numbers[i+1] = numbers[i+1], numbers[i]            
+        numbersIterated-=1
+
+if __name__ == "__main__":  
+    numbers = create_random_list(9, 1, 30)
+    print(f"Original:\n{numbers}")
+    sort(numbers)
+    print(f"Sorted:\n{numbers}")
